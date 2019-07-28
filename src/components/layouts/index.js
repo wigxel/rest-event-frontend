271e0d3be295af60b69d4cfb5ce3c3e9327691b3
Chinedum Chiemela <chiemelachinedum@gmail.com>
2019-07-28 11:14:10 +0100

    feat(vendor-card): Adds the four column grid component

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { media } from '../../styles/helpers';
 
 const JumbotronStyle = styled.div`
   min-height: calc(100vh - 80px);
   background-color: ${props => props.theme.primary};
+`;
+
+const FourColumnGridStyle = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 2rem;
+  position: relative;
+  ${media.tabletLand`
+    grid-template-columns: repeat(3, 1fr);
+  `}
+
+  ${media.tabletPort`
+    grid-template-columns: repeat(2, 1fr);
+  `}
+
+  ${media.phone`
+    grid-template-columns: repeat(1, 1fr);
+  `}
 `;
 
 export const Jumbotron = props => {
@@ -12,4 +31,8 @@ export const Jumbotron = props => {
 
 export const Container = props => {
   return <div className="container mx-auto">{props.children}</div>;
+};
+
+export const FourColumnGrid = props => {
+  return <FourColumnGridStyle {...props}>{props.children}</FourColumnGridStyle>;
 };
