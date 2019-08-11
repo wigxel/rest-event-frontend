dd9816dbadf1e18489897a51c884223f9d73d36e
Joseph Julius <Joseph.owonwo@gmail.com>
2019-08-11 20:03:31 +0100

    test: added test for currency functions
    written some test for the currency functions of the libs/numbers folder
    
    BREAKING CHANGE: CurrencyInput should have a glitch

--- src/libs/numbers/currency.js
+++ src/libs/numbers/currency.js
@@ -1,13 +1,12 @@
-// <CtryNm>NIGERIA</CtryNm>
-// <CcyNm>Naira</CcyNm>
-// <Ccy>NGN</Ccy>
-// <CcyNbr>566</CcyNbr>
-// <CcyMnrUnts>2</CcyMnrUnts>
+const defaultConfig = {
+  style: 'currency',
+  currencyDisplay: 'symbol',
+  maximumFractionDigits: 2
+};
 
-export const naira = prop =>
-  new Intl.NumberFormat('en-US', {
-    currency: 'NGN',
-    currencyDisplay: 'symbol',
-    maximumFractionDigits: 2,
-    ...prop
-  }).format;
+export const currency = (lang = 'en-US') => (config = {}) =>
+  new Intl.NumberFormat(lang, { ...defaultConfig, ...config }).format;
+
+export const usd = currency('en-US')({ currency: 'USD' });
+export const euro = currency('en-US')({ currency: 'EUR' });
+export const naira = currency('en-US')({ currency: 'NGN' });
