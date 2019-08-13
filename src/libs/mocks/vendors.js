a0eee411e5963e577158771901d05ecfb5f13155
Joseph Julius <Joseph.owonwo@gmail.com>
2019-08-13 19:38:00 +0100

    refactor: changed the mock image path for vendors
    Changed the directory for vendor images from public/images to src/assets/images

--- src/libs/mocks/vendors.js
+++ src/libs/mocks/vendors.js
@@ -1,10 +1,12 @@
 import { slugify } from '../../libs/helpers';
 
+const getImage = name => require(`../../assets/images/${name}`);
+
 export const createVendor = data => ({
   rating: 5,
   name: 'Diana\'s Buffet',
-  banner: '/images/vendor_banner.png',
-  logo: '/images/vendor_logo.png',
+  banner: getImage('vendor_banner.png'),
+  logo: getImage('vendor_logo.png'),
   isPremium: false,
   description:
     'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo hic, dolore dolorem culpa modi iste consectetur, aliquam iusto, reiciendis qui totam nostrum et blanditiis omnis. Obcaecati deleniti pariatur maxime sequi ipsum reprehenderit ut laboriosam velit architecto, rerum, inventore sunt impedit modi itaque molestiae, iusto voluptate doloribus. Nostrum debitis cum id.',
